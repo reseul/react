0b0ee879f15cb6cdfbf4d07e03a5bd2b2e01c397
Radu Simionescu <radus@microsoft.com>
2018-12-04 18:28:21 -0800

    Tweaks

--- simple/src/index.tsx
+++ simple/src/index.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import { Provider, themes } from '@stardust-ui/react'
 
 import App from './App'
 
@@ -10,9 +9,4 @@ root.id = _rootId
 
 document.body.appendChild(root)
 
-ReactDOM.render(
-  <Provider theme={themes.teams}>
-    <App />
-  </Provider>,
-  document.getElementById(_rootId),
-)
+ReactDOM.render(<App />, document.getElementById(_rootId))
